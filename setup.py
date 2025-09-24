74c0652377ffda23659885d35413152f78810a63
Mukul404 <iammukul002@gmail.com>
2025-09-24 14:14:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    mane = 'Info-retrieval-system',
+    version = '0.0.1',
+    author = 'Mukul Sharma',
+    author_email = 'iammukul002@gmail.com',
+    packages = find_packages(),
+    install_requires = [],
+
+)
+
